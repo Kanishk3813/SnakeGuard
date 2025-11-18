a206c48086b6d1f8aeec9010dd475253a818f465
Kanishk_3813 <kanishkreddy3813@gmail.com>
2025-11-18 13:16:21 +0530

    supabase migration

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['qxprluabmqdctnweebpb.supabase.co'],
+    domains: ['eplrcsnzpfjnohjkafft.supabase.co'],
   },
 };
 
