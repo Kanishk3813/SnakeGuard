deb5405249333a8ba1744d4bedb95c29df179cda
Kanishk_3813 <kanishkreddy3813@gmail.com>
2026-03-04 17:11:29 +0530

    minor fix

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,10 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'snakeguard.jiobase.com',
+      },
+      {
+        protocol: 'https',
         hostname: 'upload.wikimedia.org',
       },
       {
